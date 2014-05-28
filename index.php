afaef6610352cad1db41f419c6a8add71def6048
miton <rcd18@hotmail.fr>
2014-05-28 18:22:19 +0200

    FIRST COMMIT

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+	<head>
+		<meta charset="utf_8">
+		<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+		<meta name="viewport" content="width=device-width; initial-scale=1.0; user-scalable=no">
+		<meta name="description" content=""/>
+		<meta name="author" CONTENT="Auteur : Rémi collignon ;" />
+		<meta name="copyright" content="© rcdinfo.fr" />
+		<meta name="revisit" content="1 days" />
+
+		<script src="//ajax.googleapis.com/ajax/libs/angularjs/1.2.16/angular.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+		<script src="//netdna.bootstrapcdn.com/bootstrap/3.0.0/js/bootstrap.min.js"></script>
+
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap.min.css">
+		<link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.0.0/css/bootstrap-theme.min.css">
+
+		<title>SELECT</title>
+
+		<link rel="icon" type="image/png" href="img/fav.jpg" />
+		<!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="img/fav.jpg" /><![endif]-->		
+	</head>
+	<body>
+		<div class="container col-lg-8 col-lg-offest-2">
+		
+		</div>
+		<div id="footer">
+		</div>
+	</body>
+</html>
