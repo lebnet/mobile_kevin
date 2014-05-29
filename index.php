294e1de5e1139330aba163a266677f105ec463f9
miton <rcd18@hotmail.fr>
2014-05-29 17:58:05 +0200

    sub panel

--- index.php
+++ index.php
@@ -97,24 +97,98 @@
 					');
 				}
 				else{
-					echo('<div id="iframe" class="col-sm-12">
-							<iframe  src="http://travel.orange.fr/services_couverture.php?offre=10&destination_id=MAN&zone_code=1&ligne=dmgp" scrolling="no"></iframe>
-						  </div>
+					echo('
 					');
 
 					echo('<div class="panel-body">
 							<div class="panel-group">');
 
 
-					etape("PREREQUIS", "Avant de partir n'oubliez pas :<br/> - de vérifier que vous disposez du mode international sur votre ligne en regardant dans votre espace client > mes options sinon vous n'aurez pas de réseau à l'étranger <br/>- d'initialiser le code secret de votre messagerie vocale (appel au 888, puis choix 2 et laissez vous guider).");
-					
-					etape("EXPLICATION FONCTIONNEMENT ETRANGER", "Ensuite, il faut savoir que pour toutes les personnes restées en France (votre famille / vos amis) qui envoient des SMS ou appellent un numéro français à l'étranger (vous), cela sera facturé comme d'habitude pour lui soit en général illimité pour les SMS voir les appels en fonction de leur forfait. Par contre sans option(s), la réception d'appels te sera facturée, la réception de SMS étant gratuite.");
-					
-					etape("EXPLICATIONS PRATIQUES <b>EN " . $_POST['pays'] . "</b>", "Pour appeler la France, je fais le + 33 6... (si j'appelle un n° commençant par 06 par exemple)
-						Pour appeler en "  . $_POST['pays'] .  ", je fais le +"                .                "indicatif... suivi du n° local
-						888 ou +33 6 08 08 08 08 la messagerie vocale
-						444 ou #123# (appel gratuit depuis l'étranger) informations, options et pass'
+					etape("PREREQUIS", "<h5>Avant de partir n'oubliez pas :</h5>  
+										- de vérifier que vous disposez du mode international sur votre ligne en regardant dans votre 
+										espace client > mes options sinon vous n'aurez pas de réseau à l'étranger.<br/>
+										- d'initialiser le code secret de votre messagerie vocale (appel au 888, puis choix 2 et laissez 
+										vous guider).<br/>
+										- de  désactiver les  données  cellulaires  (internet)  sur votre portable  pour  éviter  tout  hors 
+										forfait monstrueux  si vous ne prenez pas de pass internet ou n’avez de data en itinérance 
+										incluse.
 					");
+					
+					etape("EXPLICATION FONCTIONNEMENT ETRANGER", "Ensuite, il faut savoir que pour toutes les personnes restées en France (votre famille /vos
+																amis) qui envoient des SMS ou appellent un numéro français à l'étranger (vous), cela sera 
+																facturé  comme d'habitude pour lui soit en général illimité pour les SMS voir les appels en 
+																fonction de leur forfait. Par contre sans option(s), la réception d'appels vous sera facturée, la 
+																réception de SMS étant gratuite.
+																");
+
+					echo ("<h3><b> EN " . $_POST['pays'] . ":</b></h3>");
+					
+					etape("EXPLICATIONS PRATIQUES" , "Pour appeler la France, je fais le + 33 6... (si j'appelle un n° commençant par 06 par exemple)  
+														Pour appeler en " . $_POST['pays'] . ", je fais le +indicatif... suivi du n° local  
+														888 ou +33 6 08 08 08 08 la messagerie vocale  
+														444 ou #123# (appel gratuit depuis l'étranger) informations, options et pass."
+														.'<div class="panel-body">
+															<div class="panel-group">
+																<div class="panel panel-default">
+																	<div class="panel-heading">
+																		<h4 class="panel-title">
+																			<a class="panel-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse6">Couverture et services</a>	
+																		</h4>
+																	</div>
+																	<div id="collapse6" class="panel-body collapse">
+																		<div class="panel-inner" style="text-align: justify;">
+																			iframe détourée comme celle-ci
+																				<div id="iframe" class="col-sm-12">
+																					<iframe  src="http://travel.orange.fr/services_couverture.php?offre=10&destination_id=MAN&zone_code=1&ligne=dmgp" scrolling="no"></iframe>
+						  														</div>
+																		</div>
+																	</div>
+																</div>
+
+																<div class="panel panel-default">
+																	<div class="panel-heading">
+																		<h4 class="panel-title">
+																			<a class="panel-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse7">La 4G à l\'étranger</a>	
+																		</h4>
+																	</div>
+																	<div id="collapse7" class="panel-body collapse">
+																		<div class="panel-inner" style="text-align: justify;">'
+																			."La 4G vous accompagne dans plusieurs pays avec les partenaires d'Orange ! 
+																			Les clients 4G d'Orange France ainsi que Sosh bénéficient d'ores et déjà de l'accès à la 4G en Espagne, 
+																			au Portugal, au Royaume Uni, en Belgique, aux Pays Bas, au Luxembourg, en Suisse, en Italie, au 
+																			Canada et en Corée du Sud. Bientôt, ils pourront également bénéficier de l'accès à la 4G en Allemagne. 
+																			Le tarif pour utiliser la 4G à l'étranger est le même que celui de la 3G. 
+																			Profiter du très haut débit mobile à l'étranger nécessite les mêmes conditions qu'en France 
+																			  métropolitaine : il vous suffit d'avoir un terminal compatible, d'avoir une offre 4G/H+ et d'être sous c
+																			  ouverture 4G. 
+																			La 4G à l'étranger est disponible selon l'offre souscrite, la disponibilité d'un accord de roaming 4G 
+																			  entre Orange SA et un opérateur partenaire, le support par le terminal du client des bandes de 
+																			  fréquences LTE opérées par l'opérateur partenaire et la disponibilité de la couverture 4G de  
+																			  l'opérateur partenaire.".'
+																		</div>
+																	</div>
+																</div>
+
+																<div class="panel panel-default">
+																	<div class="panel-heading">
+																		<h4 class="panel-title">
+																			<a class="panel-toggle" data-toggle="collapse" data-parent="#accordion3" href="#collapse8">Compatibilité du téléphone</a>	
+																		</h4>
+																	</div>
+																	<div id="collapse8" class="panel-body collapse">
+																		<div class="panel-inner" style="text-align: justify;">
+																			iframe (statique la par contre) détourée comme celle ci 
+																				<div id="iframe" class="col-sm-12">
+																					<iframe  src="http://travel.orange.fr/services_compatibilite.php?offre=10&destination_id=DE&zone_code=1&ligne=dmgp&" scrolling="no"></iframe>
+																				</div>
+																		</div>
+																	</div>
+																</div>
+															</div>
+														</div>
+					');
+					
+					etape('TARIFS PAR DEFAUT', 'a faire plus tard');
 
 					echo('</div></div>');
 				}
